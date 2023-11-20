a313f2b92b057ac4acf58fcd7d8b1eb88a598463
Beauclair <rileyjhumphreys@gmail.com>
2023-11-20 20:42:37 +1000

    Progress update psuh

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+
+    let modifiedArray = [...array];
+    let inputValues = values;
+
+    for (let i = 0; i <= inputValues.length; i++){ 
+        for (x = 0; x <= modifiedArray.length; x++){
+            if(modifiedArray[x] === inputValues[i]){ 
+                modifiedArray.splice(x, 1);
+            };
+        };
+    }
+    return modifiedArray;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
