57443695d0f1b43508a44ddf1dc4a961a70cf0fb
Beauclair <rileyjhumphreys@gmail.com>
2023-11-23 21:44:28 +1000

    Completed more exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function (...numbers) {
+
+    let inputNumbers = [...numbers];
+    const highestInputNumber = Math.max(...inputNumbers)
+    const lowestInputNumber = Math.min(...inputNumbers)
+
+    if(!Number.isInteger(highestInputNumber), !Number.isInteger(lowestInputNumber)){ 
+       return "ERROR";
+    }
+
+    if (lowestInputNumber === highestInputNumber) {
+        return "ERROR";
+    } else if (lowestInputNumber < 0 || highestInputNumber < 0) {
+        return "ERROR";
+    } else if (lowestInputNumber,highestInputNumber) {
+        return "ERROR";
+    }else if (lowestInputNumber < highestInputNumber) {
+        let sumNumber = 0;
+        for (i = lowestInputNumber; i <= highestInputNumber; i++) {
+            sumNumber += i;
+        }
+        return sumNumber;
+    }
 
 };
 
