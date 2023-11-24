3f75fe676b54546bda457a81be9c17215ea30e63
Beauclair <rileyjhumphreys@gmail.com>
2023-11-24 22:21:01 +1000

    Completion of exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    
+    const divideFour = inputYear % 4;
+    const divideOneHundred = inputYear % 100;
+    const divideFourHundred = inputYear % 400;
+    let isLeapYear;
 
+    if ((divideFour == 0 && !divideOneHundred == 0) || divideFourHundred ==0) {
+        isLeapYear = true;
+      } else { 
+        isLeapYear = false;
+      }
+
+      return (isLeapYear)
 };
 
 // Do not edit below this line
