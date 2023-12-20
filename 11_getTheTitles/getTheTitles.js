96a053cc7a85092c3c98cf457e0c4aa477524da3
Beauclair <rileyjhumphreys@gmail.com>
2023-12-20 22:06:55 +1000

    Completed exercise 11 get the files

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const booksTitles = books.map(book => book.title)
+    return booksTitles
 };
 
 // Do not edit below this line
