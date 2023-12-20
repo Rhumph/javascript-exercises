68f71d0a2e5e860fc64cfa14817eb3ac67e77baf
Beauclair <rileyjhumphreys@gmail.com>
2023-12-20 21:41:02 +1000

    Completed 10 Fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (fibonacciSequenceIndex) {
+
+    let fibseqind = parseInt(fibonacciSequenceIndex);
+
+    if (fibseqind < 0) {
+        return "OOPS"
+    }
+
+    let fibonacciSequenceArray = [0, 1, 1];
+
+    for (let i = 3; i <= fibseqind; i++) {
+        let nextFibonacciNumber = fibonacciSequenceArray[i - 1] + fibonacciSequenceArray[i - 2];
+        fibonacciSequenceArray.push(nextFibonacciNumber);
+    }
+
+    let selectedFibIndex = fibonacciSequenceArray[fibonacciSequenceIndex];
+    return selectedFibIndex
 
 };
 
