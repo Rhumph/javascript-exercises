3e6bc327003c843148cf92ac82e257e0bd841739
Beauclair <rileyjhumphreys@gmail.com>
2023-12-20 15:40:07 +1000

    Completed calculator -08 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedCelsius = ((fahrenheit -32) * (5/9));
+  const roundedCelsius = convertedCelsius.toFixed(1);
+  let finalCelsius = Number.parseFloat(roundedCelsius);
+  return (finalCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedFahrenheit = ((celsius * (9/5) + 32));
+  const roundedFahrenheit = convertedFahrenheit.toFixed(1);
+  let finalFahrenheit = Number.parseFloat(roundedFahrenheit);
+  return (finalFahrenheit);
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  let arr = [0, ...numbers];
+  let sumOfArray = arr.reduce((sum, current) => sum + current);
+  return sumOfArray  
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {	
+  let arr = [...numbers];
+  let subractionOfArray = arr.reduce((sum, current) => sum - current);
+  return subractionOfArray 
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {	
+  if (numbers.length === 1 && Array.isArray(numbers[0])) {
+    let sumOfArray = numbers[0].reduce((sum, current) => sum + current, 0);
+    return sumOfArray;
+  }
+  
+  let arr = [...numbers];
+  let undefinedZero = 0;
+  arr.unshift(undefinedZero);
+  let sumOfArray = arr.reduce((sum, current) => sum + current);
+  return parseInt(sumOfArray)  
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  if (numbers.length === 1 && Array.isArray(numbers[0])) {
+    let sumOfArray = numbers[0].reduce((sum, current) => sum * current);
+    return sumOfArray;
+  }
+  
+  let arr = [...numbers];
+  // let undefinedZero = 0;
+  // arr.unshift(undefinedZero);
+  let sumOfArray = arr.reduce((sum, current) => sum * current);
+  return parseInt(sumOfArray)  
 };
 
-const power = function() {
-	
+const power = function(...numbers) {
+  if (numbers.length === 1 && Array.isArray(numbers[0])) {
+    let sumOfArray = numbers[0].reduce((sum, current) => sum ** current);
+    return sumOfArray;
+  }
+  
+  let arr = [...numbers];
+  // let undefinedZero = 0;
+  // arr.unshift(undefinedZero);
+  let sumOfArray = arr.reduce((sum, current) => sum ** current);
+  return parseInt(sumOfArray) 
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+ let result = 1
+
+ for (let index = 1; index <= number; index++) {
+  result *= index;  
+ }
+ return result
 };
 
 // Do not edit below this line
