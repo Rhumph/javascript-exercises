107f37cdb732f31989bab243ae47b7ddbde0b764
Beauclair <rileyjhumphreys@gmail.com>
2023-12-20 16:28:54 +1000

    Completed 09 Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
 
+    isAPalindrome = false;
+    let functionSentence = String(sentence);
+    let lowerCaseSTring = functionSentence.toLowerCase();
+    let cleanedString = lowerCaseSTring.replace(/[^\w]|_/g, '');
+
+    let inverseString = cleanedString.split('').reverse().join('');
+
+    if (cleanedString === inverseString) { 
+        isAPalindrome =true;
+    }
+
+    // console.log(grammarFreeString + " " + inverseString)
+
+    return isAPalindrome;
 };
 
 // Do not edit below this line
