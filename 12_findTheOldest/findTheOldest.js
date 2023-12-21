e2df166b3470237eee2c2bb22d4d98d86d7252c5
Beauclair <rileyjhumphreys@gmail.com>
2023-12-21 15:07:44 +1000

    Completed TOP Exercise 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            person.age = new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
 
+    let ages = people.map(person => person.age);
+    let ageSortedList = people.sort((a, b) => a.age - b.age);
+    return ageSortedList[ageSortedList.length - 1];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
